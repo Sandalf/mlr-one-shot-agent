8a566ac96e1685ba7f8c3626f3ed8379e2b1e076
Luis Sandoval <alfsand97@gmail.com>
2020-11-28 19:12:16 -0700

    first commit

--- src/examples/behaviours/OneShotAgent.java
+++ src/examples/behaviours/OneShotAgent.java
@@ -29,43 +29,41 @@ public class OneShotAgent extends Agent {
 
         public void action() {
             Object[] args = getArguments();
+            double x1Arg = 0;
+            double x2Arg = 0;
 
-            double xArg = 0;
-            if (args != null && args.length > 0) {
-                xArg = Double.parseDouble((String) args[0]);
+            if (args != null && args.length >= 2) {
+                x1Arg = Double.parseDouble((String) args[0]);
+                x2Arg = Double.parseDouble((String) args[1]);
             } else {
-                System.out.println("You must pass an x value as an argument");
+                System.out.println("You must pass x1 and x2 values as arguments");
+                return;
             }
     
-            // Benetton data set: https://www.displayr.com/what-is-linear-regression/
-            // X: Advertising, Y: Sales
-            double[][] benettonData = {
-                {23, 651},
-                {26, 762},
-                {30, 856},
-                {34, 1063},
-                {43, 1190},
-                {48, 1298},
-                {52, 1421},
-                {57, 1440},
-                {58, 1518}
+            double[][] exampleData = {
+                {41.9, 29.1, 251.3},
+                {43.4, 29.3, 251.3},
+                {43.9, 29.5, 248.3},
+                {44.5, 29.7, 267.5},
+                {47.3, 29.9, 273},
+                {47.5, 30.3, 276.5},
+                {47.9, 30.5, 270.3},
+                {50.2, 30.7, 274.9},
+                {52.8, 30.8, 285},
+                {53.2, 30.9, 290},
+                {56.7, 31.5, 297},
+                {57,   31.7, 302.5},
+                {63.5, 31.9, 304.5},
+                {65.3, 32,   309.3},
+                {71.1, 32.1, 321.7},
+                {77,   32.5, 330.7},
+                {77.8, 32.9, 349}
             };
     
-            // Inhabited private homes in Mexico: https://www.inegi.org.mx/temas/vivienda/#Informacion_general
-            // X: Year, Y: Private homes in millions
-            double[][] inegiData = {
-                {1990, 16.18},
-                {1995, 19.40},
-                {2000, 21.94},
-                {2005, 24.70},
-                {2010, 28.60},
-                {2015, 31.94}
-            };
-    
-            Dataset dataset = new Dataset(inegiData);
-            SimpleLinearRegression slr = new SimpleLinearRegression(dataset);
-            slr.printEquation();
-            slr.predict(xArg);
+            Dataset dataset = new Dataset(exampleData);
+            MultipleLinearRegression mlr = new MultipleLinearRegression(dataset);
+            mlr.printEquation();
+            mlr.predict(x1Arg, x2Arg);
         }
 
         public int onEnd() {
